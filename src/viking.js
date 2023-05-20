74d5eb49b42431ecafba41fc6d77815bcedc4a8c
Biljana Kotevska <biljanakotev@gmail.com>
2023-05-20 13:57:08 +0200

    finished Saxon iteration

--- src/viking.js
+++ src/viking.js
@@ -32,10 +32,31 @@ class Viking extends Soldier {
       return `${this.name} has died in act of combat`;
     }
   }
+
+  battleCry() {
+    return "Odin Owns You All!";
+  }
 }
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  constructor(health, strength) {
+    super(health, strength);
+  }
+
+  attack() {
+    return this.strength;
+  }
+
+  receiveDamage(damage) {
+    this.health -= damage;
+    if (this.health > 0) {
+      return `A Saxon has received ${damage} points of damage`;
+    } else {
+      return "A Saxon has died in combat";
+    }
+  }
+}
 
 // War
 class War {}
